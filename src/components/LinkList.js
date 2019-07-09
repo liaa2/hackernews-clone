4a5188a7d72b9a0dcfba91741d1e0ec1a2921f36
Linna Liu <cello1412@gmail.com>
2019-07-09 22:19:27 +1000

    Query component done

---
+++ src/components/LinkList.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+import { Query } from 'react-apollo'
+import gql from 'graphql-tag'
+
+import Link from './Link'
+
+//The gql function is used to parse the plain string that contains the GraphQL code
+const FEED_QUERY = gql `
+  {
+    feed {
+      links {
+        id
+        createdAt
+        url
+        description
+      }
+    }
+  }
+`
+
+class LinkList extends Component {
+  render(){
+    return (
+      // fetched data from the server via <Query /> render prop function
+      // These 3 props provide information about the state of the network request:
+      /* 1. loading: Is true as long as the request is still ongoing and the response hasn’t been received.
+      2. error: In case the request fails, this field will contain information about what exactly went wrong.
+      3. data: This is the actual data that was received from the server. */
+      <Query query={FEED_QUERY}>
+        {({ loading, error, data }) => {
+          if (loading) return <div>Fetching</div>
+          if (error) return <div>Error</div>
+
+          const linksToRender = data.feed.links
+          
+          return (
+            <div>
+              {linksToRender.map(link => <Link key={link.id} link={link} />)}
+            </div>
+          ) 
+        }}   
+      </Query>
+    )
+  }
+}
+
+export default LinkList
--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './styles/index.css';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//react-apollo contains the bindings to use Apollo Client with React.
+import { ApolloProvider } from 'react-apollo';
+//apollo - boost offers some convenience by bundling several packages you need when working with Apollo Client:
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+
+//create the httpLink that will connect ApolloClient instance with the GraphQL API, GraphQL server will be running on localhost: 4000
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000'
+})
+
+//instantiate ApolloClient by passing in the httpLink and a new instance of an InMemoryCache
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+})
+
+ReactDOM.render(
+  //The App is wrapped with the higher-order component ApolloProvider that gets passed the client as a prop
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>, document.getElementById('root'));
+
 serviceWorker.unregister();
