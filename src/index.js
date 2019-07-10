49d380302cfed8ea21344855501a12f9f81f987a
Linna Liu <cello1412@gmail.com>
2019-07-10 21:17:10 +1000

    added Header component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/index.css';
-import App from './components/App';
-import * as serviceWorker from './serviceWorker';
-
+// wrap the App with BrowserRouter so that all child components of App will get access to the routing functionality
+import { BrowserRouter } from 'react-router-dom';
 //react-apollo contains the bindings to use Apollo Client with React.
 import { ApolloProvider } from 'react-apollo';
-//apollo - boost offers some convenience by bundling several packages you need when working with Apollo Client:
+// apollo - boost offers some convenience by bundling several packages you need when working with Apollo Client:
 import { ApolloClient } from 'apollo-client';
 import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+
+import App from './components/App';
+import * as serviceWorker from './serviceWorker';
+
+import './styles/index.css';
 
 
 //create the httpLink that will connect ApolloClient instance with the GraphQL API, GraphQL server will be running on localhost: 4000
@@ -24,9 +27,11 @@ const client = new ApolloClient({
 })
 
 ReactDOM.render(
-  //The App is wrapped with the higher-order component ApolloProvider that gets passed the client as a prop
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>, document.getElementById('root'));
+  <BrowserRouter>
+    {/*The App is wrapped with the higher-order component ApolloProvider that gets passed the client as a prop*/}
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>, document.getElementById('root'));
 
 serviceWorker.unregister();
